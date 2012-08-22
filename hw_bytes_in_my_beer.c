2549ea546d50419d4eedfdb84eb831274c8807fc
Teijo Laine <teijo@aropupu.fi>
2012-08-22 19:59:47 +0300

    Obfuscate more

--- hw_bytes_in_my_beer.c
+++ hw_bytes_in_my_beer.c
@@ -1,21 +1,17 @@
 #if 0
-cat $0 && gcc $0 -o b && ./b; rm ./b
-exit 0
+cat $0 && gcc $0 -o b && ./b; rm ./b; exit 0
 #endif
 #include <stdint.h>
 int main() {
     const char* s = "helo wrd";
-    const uint8_t l = strlen(s);
-    int32_t p[8];
-    int r, i, j, k, n = 0;
+    int r, i, j, k, n = 0, p[8];
     memset(p, -1, sizeof(p));
     srand('b'<<24|'e'<<16|'e'<<8|'r');
-    for (k = 0; k < l; n++, r = rand())
+    for (k = 0; k < 8; n++, r = rand())
         for (j = 0; j < sizeof(r); j++)
-            for (i = 0; i < l; i++)
+            for (i = 0; i < 8; i++)
                 if (p[i] == -1 && s[i] == ((char*)&r)[j]) {
-                    p[i] = n*4+j;
-                    printf("%c: 0x%X\n", s[i], p[i]);
+                    printf("%c: 0x%X\n", s[i], p[i] = n*4+j);
                     k++;
                 }
 }
